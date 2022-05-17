7eb7187f7a24467b61e81a164be0f4f025c89719
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-05-17 16:04:25 +0200

    Display list of item at the homepage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import css from './style.css'; // eslint-disable-line
+import Leagues from './modules/leagues.js';
 import logo from './assets/logo.png';
 
 const logoImg = document.querySelector('#logo');
 logoImg.src = `${logo}`;
+
+const newLeagues = new Leagues();
+
+window.addEventListener('load', newLeagues.fetchLeague);
