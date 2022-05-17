08c619870164cffffdfa6ad69e3597467652b6a0
Tuğsem Saranlı <saranlitugsem@gmail.com>
2022-05-17 21:21:15 +0300

    Revert "Display list of item at the homepage"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import css from './style.css'; // eslint-disable-line
-import Leagues from './modules/leagues.js';
 import logo from './assets/logo.png';
 
 const logoImg = document.querySelector('#logo');
 logoImg.src = `${logo}`;
-
-const newLeagues = new Leagues();
-
-window.addEventListener('load', newLeagues.fetchLeague);
