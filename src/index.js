96bb74368e96b521ce26fc83b286cc375f335ce4
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-05-18 12:13:11 +0200

    display numbers of likes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import css from './style.css'; // eslint-disable-line
 import Leagues from './modules/leagues.js';
+import LeagueLikes from './modules/LeagueLikes.js';
 import logo from './assets/logo.png';
 
 const logoImg = document.querySelector('#logo');
 logoImg.src = `${logo}`;
 
 const newLeagues = new Leagues();
-
-window.addEventListener('load', newLeagues.fetchLeague);
+const newLeagueLikes = new LeagueLikes();
+const checkAppId = () => { if (!localStorage.getItem('app_id')) return newLeagues.postAppId(); return null; };
+const contentBody = document.querySelector('.content-body');
+contentBody.addEventListener('click', (event) => {
+  let card = event.target.parentElement.parentElement;
+  if (event.target.tagName === 'I') {
+    card = event.target.parentElement.parentElement.parentElement;
+    newLeagueLikes.postLike(card.id);
+  }
+});
+window.addEventListener('load', () => {
+  checkAppId();
+  newLeagues.fetchLeague();
+});
