60837c7b128d36fffe69144c0f1ef3f52d5f3219
Tugsem <saranlitugsem@gmail.com>
2022-05-18 16:53:35 +0300

    Add form structure to add comments

--- src/modules/pop-up-details.js
+++ src/modules/pop-up-details.js
@@ -5,8 +5,14 @@ const getDetail = (item) => {
   popUp.style.display = 'flex';
   popUp.innerHTML = `<div class="pop-up"><div id="close">&times</div>
     <div class="pop-up-img"><img src=${item.logos.light} alt="logo"/></div>
-    <div class="card-description"><h1>${item.name}</h1></div>
+    <div class="card-name"><h1>${item.name}</h1></div>
     <div class="details"><p><b>slug: </b>${item.slug}</p><p><b>abbr: </b>${item.abbr}</p></div>
+    <div class="comment-counter">Comments</div>
+    <div class="generate-comments"></div>
+    <div class="add-comment">Add a comment</div>
+    <input type="text" id="name" placeholder="Your name">
+    <textarea id="txt-area" placeholder="Your insights"></textarea>
+    <button class="comment-button">Comment</button>
     </div>
 `;
   const btn = document.querySelector('#close');
